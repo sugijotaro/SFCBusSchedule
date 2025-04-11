55e22989f05c22e0a8b656799408ece575d164ff
Jotaro Sugiyama <sugi.jotaro@gmail.com>
2025-04-11 21:04:21 +0900

    add initializer

--- Sources/SFCBusSchedule/BusScheduleModels.swift
+++ Sources/SFCBusSchedule/BusScheduleModels.swift
@@ -12,10 +12,40 @@ public struct BusSchedule: Codable, Identifiable {
     public let destination: String
     public let via: String
     public let metadata: Metadata
+
+    public init(
+        id: String,
+        time: Int,
+        minute: Int,
+        scheduleType: String,
+        routeCode: String,
+        routeName: String,
+        name: String,
+        origin: String,
+        destination: String,
+        via: String,
+        metadata: Metadata
+    ) {
+        self.id = id
+        self.time = time
+        self.minute = minute
+        self.scheduleType = scheduleType
+        self.routeCode = routeCode
+        self.routeName = routeName
+        self.name = name
+        self.origin = origin
+        self.destination = destination
+        self.via = via
+        self.metadata = metadata
+    }
 }
 
 public struct Metadata: Codable {
     public let stops: [Stop]
+
+    public init(stops: [Stop]) {
+        self.stops = stops
+    }
 }
 
 public struct Stop: Codable {
@@ -28,9 +58,20 @@ public struct Stop: Codable {
         case cumulativeTime = "cumulative_time"
         case arrival
     }
+
+    public init(name: String, cumulativeTime: Int, arrival: Arrival) {
+        self.name = name
+        self.cumulativeTime = cumulativeTime
+        self.arrival = arrival
+    }
 }
 
 public struct Arrival: Codable {
     public let time: Int
     public let minute: Int
+
+    public init(time: Int, minute: Int) {
+        self.time = time
+        self.minute = minute
+    }
 } 
